03a64b787218838cd6c7d2656b2ffd52045b2cb9
sharozraees802 <sharozraees802@gmail.com>
2022-10-17 16:21:48 +0500

    mistake in project description solve

--- utils/projects.js
+++ utils/projects.js
@@ -5,7 +5,7 @@ const projects = {
             image: '/assets/projects/P1.png',
             label: 'Featured Project',
             title: 'Final Year Project',
-            description: 'We tried our best to take the normal shopping experience websites to a whole new level by adding Augmented reality to them. We also design 3D models of our every product so users can see what exactly they are going to buy. We have created a project that can help people to shop online. It shows people how the product they are purchasing is going to look on them. Basically, it collects data in real time and combines it with products AR models. We have made both website and a mobile app for our product. It also contains four different product categories (necklaces, crowns, earrings, and glasses) that our users can enjoy. Allah Almighty and due to grace we also are nominated for the P@SHA ICT Awards 202 in the Category: Students -Tertiary',
+            description: 'We tried our best to take the normal shopping experience websites to a whole new level by adding Augmented reality to them. We also design 3D models of our every product so users can see what exactly they are going to buy. We have created a project that can help people to shop online. It shows people how the product they are purchasing is going to look on them. Basically, it collects data in real time and combines it with products AR models. We have made both website and a mobile app for our product. It also contains four different product categories (necklaces, crowns, earrings, and glasses) that our users can enjoy. Allah Almighty and due to grace we also are nominated for the P@SHA ICT Awards 2022 in the Category: Students -Tertiary',
             techs: [
                 'Next Js',
                 'Firebase',
