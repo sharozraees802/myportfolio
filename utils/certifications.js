af55db79c0a83361a52328c040228d62cae0cf2f
sharozraees802 <sharozraees802@gmail.com>
2022-10-26 16:35:53 +0500

    add a new certificate in a protfolio

--- utils/certifications.js
+++ utils/certifications.js
@@ -62,8 +62,17 @@ const certifications = {
             title: 'MERN STACK MASTER',
             platform: 'Udemey',
             link: 'https://www.udemy.com/share/102Rwq3@uh2VempDVML8Xaj78F9aAWcAazn8VjdYL-q3gd7fkynFL9V2uk_MFSqoOm28pmYDgQ==/',
-            date: 'Issued Sep 2002 · No Expiration Date',
+            date: 'Issued Sep 2020 · No Expiration Date',
             logo: '/assets/Udemy2.png',
+            aos: 'zoom-out-left'
+        },
+        {
+            size: 1,
+            title: 'Basics Of Kubernetes',
+            platform: '10Pearls University',
+            link: 'https://10pearlsuniversity.org/courses/basics-of-kubernetes/',
+            date: 'Issued Oct 2022 · No Expiration Date',
+            logo: '/assets/10pearlsuniversity.png',
             aos: 'zoom-out-left'
         },
     ],
