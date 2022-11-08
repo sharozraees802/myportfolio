7f5c09b7a19a3ce80a4f920f30adada410e19eff
sharozraees802 <sharozraees802@gmail.com>
2022-11-08 16:10:38 +0500

    add new open source contribution and also add a cv

--- utils/mapsContribution.js
+++ utils/mapsContribution.js
@@ -15,6 +15,19 @@ const mapsContribution = {
       iframe: "https://www.youtube.com/embed/YGoDkcuu1tE",
       iframeTitle: "YouTube video player",
     },
+    {
+      logo: "/assets/npm.png",
+      title: "@lastcode802/glassarview",
+      description: `Offer glasses virtual try-on to your users with this JavaScript widget. It can be easily integrated into an e-commerce website or into a mobile web application with a few lines of code. The experience is in real-time: the user see his face like in a mirror, but with glasses on.`,
+      paragraph: `
+      Hey folks.
+      Hopping that everyone is doing great.
+      Their Big news we want to share with all of you. We are thrilled to introduce our new library, named glassarview, as we pledged to continue our contributions to open source. This package now allows your users to virtually try on glasses. In essence, it is a library powered by computer vision that plots different 3d models on your face or body. This library now features glasses, but as you are all aware, we never stop working on new projects, thus more 3D models will eventually be included with this package. Please share this library with your friends and tech groups as we need your help in every way, also you can support us by contributing to the codebase. These things motivate us to develop more projects like this and contribute to open source. 
+      We are excited to see your response.
+    `,
+      iframe: "https://www.youtube.com/embed/HbehFa9bJGM",
+      iframeTitle: "YouTube video player",
+    },
   ],
 };
 
