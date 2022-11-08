8c4ce28c22e84064aa09adb6de7273906e09d49e
sharozraees802 <sharozraees802@gmail.com>
2022-11-08 17:10:25 +0500

    resrt

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   swcMinify: true,
   experimental: {
     images: {
